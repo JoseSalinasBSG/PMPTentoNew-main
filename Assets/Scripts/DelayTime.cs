2c485099403cb1cb631d4219c45b82868e1eb3cd
bsg-fflores <ffloresr@bsginstitute.com>
2024-11-21 16:32:41 -0500

    fix: LoadingController

--- Assets/Scripts/DelayTime.cs
+++ Assets/Scripts/DelayTime.cs
@@ -4,34 +4,19 @@ using UnityEngine.Events;
 
 public class DelayTime : MonoBehaviour
 {
-    // [SerializeField] private float _delayTimeForStart;
-    [SerializeField] private GameObject LoadingScreen;
-    // [SerializeField] private UnityEvent OnBeforeDelay;
-    // [SerializeField] private UnityEvent OnAfterDelay;
-
-    // private IEnumerator DelayRoutine()
-    // {
-    //     OnBeforeDelay?.Invoke();
-    //     yield return new WaitForSeconds(_delayTimeForStart);
-    //     OnAfterDelay?.Invoke();
-    // }
-
-    public void LoadScene(int index)
+    [SerializeField] private float _delayTimeForStart;
+    [SerializeField] private UnityEvent OnBeforeDelay;
+    [SerializeField] private UnityEvent OnAfterDelay;
+    
+    private IEnumerator DelayRoutine()
     {
-        StartCoroutine(LoadSceneAsync(index));
+        OnBeforeDelay?.Invoke();
+        yield return new WaitForSeconds(_delayTimeForStart);
+        OnAfterDelay?.Invoke();
     }
     
-    private IEnumerator LoadSceneAsync(int indexScene)
+    public void StartDelayTime()
     {
-        AsyncOperation loadingOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(indexScene);
-        
-        LoadingScreen.SetActive(true);
-        
-        while (!loadingOperation.isDone)
-        {
-            if (loadingOperation.progress >= 0.9f)
-                break;
-            yield return null;
-        }
+        StartCoroutine(DelayRoutine());
     }
 }
