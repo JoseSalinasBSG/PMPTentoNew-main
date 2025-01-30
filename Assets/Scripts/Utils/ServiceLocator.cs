fc4171aa6a60e900fc80b91f48b84a670788521a
bsg-fflores <ffloresr@bsginstitute.com>
2025-01-30 17:35:41 -0500

    update: ServiceLocator added

---
+++ Assets/Scripts/Utils/ServiceLocator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Utils
+{
+    public class ServiceLocator
+    {
+        private static ServiceLocator _instance;
+        public static ServiceLocator Instance => _instance ??= new ServiceLocator();
+        private readonly Dictionary<Type, object> _services;
+
+        public ServiceLocator()
+        {
+            _services = new Dictionary<Type, object>();
+        }
+
+        public void Register<T>(object service)
+        {
+            var type = typeof(T);
+
+            if (_services.ContainsKey(type))
+            {
+                _services[type] = service;
+                return;
+            }
+
+            _services.Add(type, service);
+        }
+
+        public T Get<T>()
+        {
+            var type = typeof(T);
+            if (!_services.TryGetValue(type, out var service))
+            {
+                throw new Exception($"Service of type {type} not found");
+            }
+
+            return (T)service;
+        }
+    }
+}
