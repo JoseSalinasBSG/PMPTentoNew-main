ee49d4c666498cea6b5ad7ea0a13b51969cedc44
JoseSalinasBSG <jsalinasv@bsginstitute.com>
2025-01-30 19:09:13 -0500

    Update post-build.bash

--- Assets/post-build.bash
+++ Assets/post-build.bash
@@ -2,10 +2,13 @@
 echo "Uploading IPA to Appstore Connect..."
 echo "Usuario: $ITUNES_USERNAME"
 echo "Contraseña: $ITUNES_PASSWORD"
-echo "xcrun altool --upload-app --type ios -f \"$path\" -u \"$ITUNES_USERNAME\" -p \"$ITUNES_PASSWORD\""
-
+echo "path: $UNITY_PLAYER_PATH"
+echo "worksapce: $WORKSPACE"
+echo "target name: $TARGET_NAME"
+#path="$WORKSPACE/.build/last/$TARGET_NAME/build.ipa"
+#Path is "/BUILD_PATH/<ORG_ID>.<PROJECT_ID>.<BUILD_TARGET_ID>/.build/last/<BUILD_TARGET_ID>/build.ipa"
 path="${UNITY_PLAYER_PATH}"
-if xcrun altool --upload-app --type ios -f "$path" -u "$ITUNES_USERNAME" -p "$ITUNES_PASSWORD" -v; then
+if xcrun altool --upload-app --type ios -f $path -u $ITUNES_USERNAME -p $ITUNES_PASSWORD ; then
     echo "Upload IPA to Appstore Connect finished with success"
 else
     echo "Upload IPA to Appstore Connect failed"
