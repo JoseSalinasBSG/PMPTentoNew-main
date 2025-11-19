92e84a590655d0f61fc658b89ac01c107801bf33
bsg-fflores <ffloresr@bsginstitute.com>
2025-11-19 11:54:28 -0500

    navbar with button active fixed

--- Assets/Scripts/UI/NavbarButtons.cs
+++ Assets/Scripts/UI/NavbarButtons.cs
@@ -9,6 +9,23 @@ public class NavbarButtons : MonoBehaviour
     [SerializeField] private UnityEngine.UI.Button _achievementButton;
     [SerializeField] private UnityEngine.UI.Button _rankingButton;
 
+    [SerializeField] private GameObject _mainButtonActive;
+    [SerializeField] private GameObject _shopButtonActive;
+    [SerializeField] private GameObject _trainButtonActive;
+    [SerializeField] private GameObject _achievementButtonActive;
+    [SerializeField] private GameObject _rankingButtonActive;
+
+    private void Start()
+    {
+        _mainButtonActive.SetActive(true);
+        _shopButtonActive.SetActive(false);
+        _trainButtonActive.SetActive(false);
+        _achievementButtonActive.SetActive(false);
+        _rankingButtonActive.SetActive(false);
+    }
+
+
+
     public enum Buttons
     {
         main,
@@ -21,12 +38,63 @@ public class NavbarButtons : MonoBehaviour
     {
         UIEvents.StartFooterButtonAnimation += UIEvents_StartFooterButtonAnimation;
         UIEvents.EndFooterButtonAnimation += UIEvents_EndFooterButtonAnimation;
+
+        _mainButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
+        {
+            _mainButtonActive.SetActive(true);
+            _shopButtonActive.SetActive(false);
+            _trainButtonActive.SetActive(false);
+            _achievementButtonActive.SetActive(false);
+            _rankingButtonActive.SetActive(false);
+        });
+
+        _shopButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
+        {
+            _mainButtonActive.SetActive(false);
+            _shopButtonActive.SetActive(true);
+            _trainButtonActive.SetActive(false);
+            _achievementButtonActive.SetActive(false);
+            _rankingButtonActive.SetActive(false);
+        });
+
+        _trainButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
+        {
+            _mainButtonActive.SetActive(false);
+            _shopButtonActive.SetActive(false);
+            _trainButtonActive.SetActive(true);
+            _achievementButtonActive.SetActive(false);
+            _rankingButtonActive.SetActive(false);
+        });
+
+        _achievementButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
+        {
+            _mainButtonActive.SetActive(false);
+            _shopButtonActive.SetActive(false);
+            _trainButtonActive.SetActive(false);
+            _achievementButtonActive.SetActive(true);
+            _rankingButtonActive.SetActive(false);
+        });
+
+        _rankingButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
+        {
+            _mainButtonActive.SetActive(false);
+            _shopButtonActive.SetActive(false);
+            _trainButtonActive.SetActive(false);
+            _achievementButtonActive.SetActive(false);
+            _rankingButtonActive.SetActive(true);
+        });
     }
 
     private void OnDisable()
     {
         UIEvents.StartFooterButtonAnimation -= UIEvents_StartFooterButtonAnimation;
         UIEvents.EndFooterButtonAnimation -= UIEvents_EndFooterButtonAnimation;
+
+        _mainButton.GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
+        _shopButton.GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
+        _trainButton.GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
+        _achievementButton.GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
+        _rankingButton.GetComponent<UnityEngine.UI.Button>().onClick.RemoveAllListeners();
     }
 
     private void UIEvents_EndFooterButtonAnimation()
